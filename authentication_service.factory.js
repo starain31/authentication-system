cf36a9e515c130b6f904e18a6421e6d5b1f6fbf4
nazmus sakib <nsakib.cse@gmail.com>
2024-02-11 23:22:58 +0600

    Add user authentication and login functionality

--- authentication_service.factory.js
+++ authentication_service.factory.js
@@ -1,9 +1,18 @@
 function create_authentication_service({ database_service }) {
     function register(params) {
+        database_service.create_user(params);
         return true;
     }
 
     function login(params) {
+        const user = database_service.get_user(params);
+        if (!user) {
+            throw new Error('Invalid username');
+        }
+
+        if (user.password !== params.password) {
+            throw new Error('Invalid password');
+        }
         return "token";
     }
 
--- authenticatoin_service.factory.spec.js
+++ authenticatoin_service.factory.spec.js
@@ -1,28 +1,44 @@
 const { create_authentication_service } = require('./authentication_service.factory');
 
-function create_mock_database_service() { }
+function create_mock_database_service() {
+    const users = {};
+
+    function create_user(params) {
+        return users[params.username] = params;
+    }
+
+    function get_user(params) {
+        return users[params.username];
+    }
+
+    return {
+        create_user,
+        get_user,
+    };
+}
 
 describe('Service: authenticationService', function () {
-    let authentication_service;
-
-    beforeAll(function () {
-        const database_service = create_mock_database_service();
-        authentication_service = create_authentication_service({ database_service });
-    });
-
     it('should be defined', function () {
+        const database_service = create_mock_database_service();
+        const authentication_service = create_authentication_service({ database_service });
         expect(authentication_service).toBeDefined();
     });
 
     it('should have a login method', function () {
+        const database_service = create_mock_database_service();
+        const authentication_service = create_authentication_service({ database_service });
         expect(authentication_service.login).toBeDefined();
     });
 
     it('should have a register method', function () {
+        const database_service = create_mock_database_service();
+        const authentication_service = create_authentication_service({ database_service });
         expect(authentication_service.register).toBeDefined();
     });
 
     it('should register a user and able to login', function () {
+        const database_service = create_mock_database_service();
+        const authentication_service = create_authentication_service({ database_service });
         const username = 'test_user_1';
         const password = 'test_user_1_password';
         const email = 'test@test.com';
@@ -46,5 +62,32 @@ describe('Service: authenticationService', function () {
 
         expect(typeof login_result).toBe('string');
     });
+
+    it('should not log in with invalid credentials', function () {
+        const database_service = create_mock_database_service();
+        const authentication_service = create_authentication_service({ database_service });
+
+        const username = 'test_user_1';
+        const password = 'test_user_1_password';
+        const email = '';
+
+        const register_params = {
+            username,
+            password,
+            email,
+        };
+
+        const result = authentication_service.register(register_params);
+
+        const wrong_login_params = {
+            username,
+            password: 'wrong_password',
+        };
+
+        expect(function () {
+            authentication_service.login(wrong_login_params);
+        }).toThrow();
+    });
+
 
 }); 
