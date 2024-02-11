e494283bf47bd706c0e68d67f8afc9f2901495c6
nazmus sakib <nsakib.cse@gmail.com>
2024-02-11 22:38:54 +0600

    Add API endpoint for system information

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
 	res.send("Hello, Node.js!");
 });
 
+app.get('/api/info', (req, res) => {
+	res.json({
+		name: 'Authentication system',
+		version: '1.0.0',
+		description: 'Authentication system',
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Authentication system listening on port ${port}`);
 });
