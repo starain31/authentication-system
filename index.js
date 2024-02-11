672ad5ad570d30908321c91c5a84bd025350f2a6
nazmus sakib <nsakib.cse@gmail.com>
2024-02-11 22:35:07 +0600

    Fix missing semicolon in index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send("Hello, Node.js!");
-})
+});
 
 app.listen(port, () => {
 	console.log(`Authentication system listening on port ${port}`);
-})
+});
